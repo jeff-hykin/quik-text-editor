a89cf92b5a978a604d0c0d29d10b57cc99a2b7bd
Jeff Hykin <jeffhykin@Jeffs-MacBook-Pro-2.local>
2018-12-18 12:54:42 -0600

    _init

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+    generateFrontend : () => `
+        import 'monaco-editor/esm/vs/editor/browser/controller/coreCommands.js'
+        import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js'
+
+        let jsonWorker       = new Worker('./node_modules/monaco-editor/esm/vs/language/json/json.worker.js')
+        let cssWorker        = new Worker('./node_modules/monaco-editor/esm/vs/language/css/css.worker.js')
+        let htmlWorker       = new Worker('./node_modules/monaco-editor/esm/vs/language/html/html.worker.js')
+        let typescriptWorker = new Worker('./node_modules/monaco-editor/esm/vs/language/typescript/ts.worker.js')
+        let editorWorker     = new Worker('./node_modules/monaco-editor/esm/vs/editor/editor.worker.js')
+
+        self.MonacoEnvironment = {
+            getWorker: function (moduleId, label) {
+                if (label === 'json') {
+                    return jsonWorker
+                }
+                if (label === 'css') {
+                    return cssWorker
+                }
+                if (label === 'html') {
+                    return htmlWorker
+                }
+                if (label === 'typescript' || label === 'javascript') {
+                    return typescriptWorker
+                }
+                return editorWorker
+            }
+        }
+        window.monaco = monaco
+    `
+}
